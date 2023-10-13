beaac94d08f8fe986760c7fc01784e6336f73f54
Key206 <khoanm206@gmail.com>
2023-10-13 15:02:25 +0700

    lec00 Makefile

---
+++ lec00/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"hello.h"
+
+int main(){
+    helloworld(); 
+    helloKhoa(); 
+    return 0;
+}
---
+++ lec00/src/hello_Khoa.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include"hello.h"
+
+void helloKhoa(){
+    printf("hello Khoa\n");
+}
